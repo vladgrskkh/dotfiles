dd6f1663adcd60412fc85ed518c8fc4088da36e3
peshkanavalnogo <playvladosik@gmail.com>
2026-01-31 22:59:23 +0300

    obsidian.nvim: add keymaps for note taking setup

--- nvim/.config/nvim/lua/vladgrskkh/plugins/mini.lua
+++ nvim/.config/nvim/lua/vladgrskkh/plugins/mini.lua
@@ -1,100 +1,106 @@
 return {
-    {"echasnovski/mini.nvim", version = false },
-   -- Comments
-   {
-       'echasnovski/mini.comment',
-       version = false,
-       dependencies = {
-           "JoosepAlviste/nvim-ts-context-commentstring",
-       },
-       config = function()
-           require('ts_context_commentstring').setup {
-               enable_autocmd = false,
-           }
-           require("mini.comment").setup {
-               options = {
-                   custom_commentstring = function()
-                       return require('ts_context_commentstring.internal').calculate_commentstring({ key =
-                            'commentstring' })
-                            or vim.bo.commentstring
-                    end,
-               },
-           }
-       end
-   },
-   -- File explorer
-   {
-       'echasnovski/mini.files',
-       config = function()
-           local MiniFiles = require("mini.files")
-           MiniFiles.setup({
-               mappings = {
-                   go_in = "<CR>",
-                   go_in_plus = "L",
-                   go_out = "-",
-                   go_out_plus = "H",
-               },
-           })
-           vim.keymap.set("n", "<leader>ee", "<cmd>lua MiniFiles.open()<CR>", { desc = "Toggle mini file explorer" })
-           vim.keymap.set("n", "<leader>ef", function()
-               MiniFiles.open(vim.api.nvim_buf_get_name(0), false)
-               MiniFiles.reveal_cwd()
-           end, { desc = "Toggle into currently opened file" })
-       end
-   },
-   {
-       'echasnovski/mini.surround',
-       event = { "BufReadPre", "BufNewFile" },
-       opts = {
-           custom_surroundings = nil,
-           highlight_duration = 300,
-           mappings = {
-               add = 'sa',
-               delete = 'ds',
-               find = 'sf',
-               find_left = 'sF',
-               highlight = 'sh',
-               replace = 'sr',
-               update_n_lines = 'sn',
+	{ "echasnovski/mini.nvim", version = false },
+	-- Comments
+	{
+		"echasnovski/mini.comment",
+		version = false,
+		dependencies = {
+			"JoosepAlviste/nvim-ts-context-commentstring",
+		},
+		config = function()
+			require("ts_context_commentstring").setup({
+				enable_autocmd = false,
+			})
+			require("mini.comment").setup({
+				options = {
+					custom_commentstring = function()
+						return require("ts_context_commentstring.internal").calculate_commentstring({
+							key = "commentstring",
+						}) or vim.bo.commentstring
+					end,
+				},
+			})
+		end,
+	},
+	-- File explorer
+	{
+		"echasnovski/mini.files",
+		config = function()
+			local MiniFiles = require("mini.files")
+			MiniFiles.setup({
+				mappings = {
+					go_in = "<CR>",
+					go_in_plus = "L",
+					go_out = "-",
+					go_out_plus = "H",
+				},
+			})
+			vim.keymap.set("n", "<leader>ee", "<cmd>lua MiniFiles.open()<CR>", { desc = "Toggle mini file explorer" })
+			vim.keymap.set("n", "<leader>ef", function()
+				MiniFiles.open(vim.api.nvim_buf_get_name(0), false)
+				MiniFiles.reveal_cwd()
+			end, { desc = "Toggle into currently opened file" })
+		end,
+	},
+	{
+		"echasnovski/mini.surround",
+		event = { "BufReadPre", "BufNewFile" },
+		opts = {
+			custom_surroundings = nil,
+			highlight_duration = 300,
+			mappings = {
+				add = "sa",
+				delete = "ds",
+				find = "sf",
+				find_left = "sF",
+				highlight = "sh",
+				replace = "sr",
+				update_n_lines = "sn",
 
-               suffix_last = 'l',
-               suffix_next = 'n',
-           },
+				suffix_last = "l",
+				suffix_next = "n",
+			},
 
-           n_lines = 20,
-           respect_selection_type = false,
-           search_method = 'cover',
-           silent = false,
-       }
-   },
-   {
-       "echasnovski/mini.trailspace",
-       event = { "BufReadPost", "BufNewFile" },
-       config = function()
-           local miniTrailspace = require("mini.trailspace")
-           miniTrailspace.setup({
-               only_in_normal_buffers = true,
-           })
-           vim.keymap.set("n", "<leader>cw", function() miniTrailspace.trim() end, { desc = "Erase Whitespace" })
-           vim.api.nvim_create_autocmd("CursorMoved", {
-               pattern = "*",
-               callback = function()
-                   require("mini.trailspace").unhighlight()
-               end,
-           })
-       end
-   },
-   {
-       "echasnovski/mini.splitjoin",
-       config = function()
-           local miniSplitJoin = require("mini.splitjoin")
+			n_lines = 20,
+			respect_selection_type = false,
+			search_method = "cover",
+			silent = false,
+		},
+	},
+	{
+		"echasnovski/mini.trailspace",
+		event = { "BufReadPost", "BufNewFile" },
+		config = function()
+			local miniTrailspace = require("mini.trailspace")
+			miniTrailspace.setup({
+				only_in_normal_buffers = true,
+			})
+			vim.keymap.set("n", "<leader>cw", function()
+				miniTrailspace.trim()
+			end, { desc = "Erase Whitespace" })
+			vim.api.nvim_create_autocmd("CursorMoved", {
+				pattern = "*",
+				callback = function()
+					require("mini.trailspace").unhighlight()
+				end,
+			})
+		end,
+	},
+	{
+		"echasnovski/mini.splitjoin",
+		config = function()
+			local miniSplitJoin = require("mini.splitjoin")
 
-           miniSplitJoin.setup({
-               mappings = { toggle = "" },
-           })
+			miniSplitJoin.setup({
+				mappings = { toggle = "" },
+			})
 
-           vim.keymap.set({"n", "x"}, "sj", function() miniSplitJoin.join() end, { desc = "Join arguments" })
-           vim.keymap.set({"n", "x"}, "sk", function() miniSplitJoin.split() end, { desc = "Split arguments" })
-       end
-   }
+			vim.keymap.set({ "n", "x" }, "sj", function()
+				miniSplitJoin.join()
+			end, { desc = "Join arguments" })
+			vim.keymap.set({ "n", "x" }, "sk", function()
+				miniSplitJoin.split()
+			end, { desc = "Split arguments" })
+		end,
+	},
 }
--- nvim/.config/nvim/lua/vladgrskkh/plugins/obsidian.lua
+++ nvim/.config/nvim/lua/vladgrskkh/plugins/obsidian.lua
@@ -1,7 +1,7 @@
 return {
 	"obsidian-nvim/obsidian.nvim",
 	version = "*", -- use latest release, remove to use latest commit
-	ft = "markdown",
+	ft = "*",
 	dependencies = {
 		"hrsh7th/nvim-cmp",
 		"MeanderingProgrammer/render-markdown.nvim",
@@ -10,6 +10,7 @@ return {
 	---@module 'obsidian'
 	---@type obsidian.config
 	opts = {
+
 		legacy_commands = false, -- this will be removed in the next major release
 		workspaces = {
 			{
@@ -18,4 +19,57 @@ return {
 			},
 		},
 	},
+	config = function()
+		local obsidian = require("obsidian")
+
+		obsidian.setup({
+			legacy_commands = false, -- this will be removed in the next major release
+			workspaces = {
+				{
+					name = "general",
+					path = "~/obsidian/notes",
+				},
+			},
+
+			templates = {
+				folder = "templates",
+				date_format = "%Y-%m-%d-%a",
+				time_format = "%H:%M",
+				customizations = {
+					daily = {
+						notes_subdir = "daily",
+						note_id_func = function(title)
+							local name = os.date("%Y-%m-%d")
+							return name
+						end,
+					},
+					quick = {
+						notes_subdir = "quick",
+					},
+				},
+			},
+
+			vim.keymap.set("n", "<leader>od", function()
+				local file = vim.fn.expand(string.format("~/obsidian/notes/daily/%s.md", os.date("%Y-%m-%d")))
+				local exists = vim.fn.filereadable(file)
+				if exists == 1 then
+					print(string.format("<cmd>edit%s<CR>", file))
+					return string.format("<cmd>edit %s<CR>", file)
+				else
+					local title = vim.fn.input("Enter a title(could be empty): ")
+					return string.format("<cmd>Obsidian new_from_template %s daily.md<CR>", title)
+				end
+			end, { expr = true, desc = "Create new daily note" }),
+
+			vim.keymap.set("n", "<leader>oq", function()
+				local title = vim.fn.input("Enter a title(could be empty): ")
+				return string.format("<cmd>Obsidian new_from_template %s quick.md<CR>", title)
+			end, { expr = true, desc = "Create new quick note" }),
+
+			vim.keymap.set("n", "<leader>ob", function()
+				local title = vim.fn.input("Enter a title(could be empty): ")
+				return string.format("<cmd>Obsidian new_from_template %s base.md<CR>", title)
+			end, { expr = true, desc = "Create new base note" }),
+		})
+	end,
 }
